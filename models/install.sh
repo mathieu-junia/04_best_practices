6e7dd51b0f18250aa3f599787061dc317b1001a1
vorph <klimczak.mathieu@pm.me>
2025-02-12 18:46:42 +0100

    feat: initial commit

---
+++ models/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+VERSION="1.2.2"
+curl -LO "https://github.com/oras-project/oras/releases/download/v${VERSION}/oras_${VERSION}_linux_amd64.tar.gz"
+mkdir -p oras-install/
+tar -zxf oras_${VERSION}_*.tar.gz -C oras-install/
+sudo mv oras-install/oras /usr/local/bin/
+rm -rf oras_${VERSION}_*.tar.gz oras-install/
